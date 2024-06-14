d3f4f145c2a82c6dc8947eeccf9a215377722946
mohamed kamel <midokamel1111@gmail.com>
2024-06-14 05:26:09 +0300

    changes

--- app/page.jsx
+++ app/page.jsx
@@ -1,6 +1,5 @@
 import Hero from "./_components/Hero";
-import ProductSection from "./_components/Productsection";
-
+import ProductSection from "./_components/ProductSection";
 export default function Home() {
 return (
 <div>
