c4a1819133cb2fa1a8d6de10025e6b64765fb7a0
mohamed kamel <midokamel1111@gmail.com>
2024-06-19 06:31:21 +0300

    custom footer

--- app/_components/Footer.jsx
+++ app/_components/Footer.jsx
@@ -1,15 +1,29 @@
 "use client";
 
-import { useUser } from '@clerk/nextjs';
-import React from 'react'
+import { useUser } from "@clerk/nextjs";
+import { Heart } from "lucide-react";
+import React from "react";
 
 function Footer() {
-  const {user} =  useUser();
-  return user&&(
-    <div>
-      Footer
-    </div>
-  )
+  const { user } = useUser();
+  return (
+    user && (
+      <div className="my-10">
+        <div className=" flex justify-center items-center">
+          <h1 className="flex gap-1">
+            Made with
+            <span>
+              <Heart className="text-red-600" />
+            </span>
+            By
+            <span className="text-blue-700 hover:text-rose-700 duration-700">
+              <a href="https://github.com/devmoekamel"> Mohamed Kamel</a>
+            </span>
+          </h1>
+        </div>
+      </div>
+    )
+  );
 }
 
-export default Footer
+export default Footer;
